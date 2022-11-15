3a5c3e383e0bff1cdf37dfd41f560ab58de64133
Nelda <101065369+cherifad@users.noreply.github.com>
2022-11-15 13:29:30 +0100

    full

---
+++ assets/js/main.js
@@ -0,0 +1,92 @@
+console.clear();
+document.getElementById('current-year').innerHTML = new Date().getFullYear();
+
+const shieldNumber1 = document.querySelector("#shield-number-1");
+const shieldNumber2 = document.querySelector("#shield-number-2");
+const submitButton = document.querySelector("#form-submit");
+const userInput = document.querySelector("#spam-shield-input");
+const emailInput = document.querySelector("#con-form-email");
+
+function create(tag, parent, text, classs = null, id = null) {
+  let temp = document.createElement(tag);
+  parent.appendChild(temp);
+  temp.innerHTML = text;
+  if (classs) temp.classList.add(classs);
+  if (id) temp.id = id;
+  return temp;
+}
+
+var result = -1;
+
+function shieldGenerator() {
+  let shieldNumber = Math.floor(Math.random() * 10) + 1;
+  shieldNumber1.innerHTML = shieldNumber;
+  result = shieldNumber;
+  shieldNumber = Math.floor(Math.random() * 10) + 1;
+  shieldNumber2.innerHTML = shieldNumber;
+  result += shieldNumber;
+}
+
+const validateEmail = (email) => {
+  return String(email)
+    .toLowerCase()
+    .match(
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    );
+};
+
+function checkResult() {
+  if (userInput.value == result && validateEmail(emailInput.value)) {
+    submitButton.style.cursor = "pointer";
+    submitButton.style.opacity = "1";
+    console.log("true");
+  } else if (userInput.value != result || !validateEmail(emailInput.value)) {
+    submitButton.style.cursor = "not-allowed";
+    submitButton.style.opacity = "0.5";
+  }
+
+  if (userInput.value == result) {    
+    userInput.style.borderColor = "#969696";
+  }
+
+  if (validateEmail(emailInput.value)) {
+    emailInput.style.borderColor = "#969696";
+  }
+}
+
+shieldGenerator();
+checkResult();
+
+userInput.addEventListener("input", () => {
+  checkResult();
+});
+
+emailInput.addEventListener("input", () => {
+  checkResult();
+});
+
+submitButton.addEventListener("click", () => {
+  if (submitButton.style.cursor == "pointer") {
+    submitButton.innerHTML = "Email envoyé !";
+  } else if(userInput.value != result && !validateEmail(emailInput.value)){
+    submitButton.classList.add("squeeze");
+    userInput.style.borderColor = "red";
+    emailInput.style.borderColor = "red";
+    setTimeout(() => {
+      submitButton.classList.remove("squeeze");
+    }, 400);
+  }
+  else if (userInput.value != result) {
+    submitButton.classList.add("squeeze");
+    userInput.style.borderColor = "red";
+    setTimeout(() => {
+      submitButton.classList.remove("squeeze");
+    }, 400);
+  } else if (!validateEmail(emailInput.value)) {
+    submitButton.classList.add("squeeze");
+    emailInput.style.borderColor = "red";
+    setTimeout(() => {
+      submitButton.classList.remove("squeeze");
+    }, 400);
+  }
+});
