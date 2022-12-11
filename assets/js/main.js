e9f08ccc50133d8d9673e8d4d800a686aaa2fc68
cherifad <95074031+Nelda74@users.noreply.github.com>
2022-12-11 15:51:27 +0100

    add files

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,6 @@
 console.clear();
 document.getElementById('current-year').innerHTML = new Date().getFullYear();
+var scroll = new SmoothScroll('a[href*="#"]');
 
 const shieldNumber1 = document.querySelector("#shield-number-1");
 const shieldNumber2 = document.querySelector("#shield-number-2");
@@ -90,3 +91,59 @@ submitButton.addEventListener("click", () => {
     }, 400);
   }
 });
+
+let mybutton = document.getElementById("back-to-top");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+} 
+
+function createPopup(html) {
+  // Create the popup container
+  var popup = document.createElement("div");
+  popup.classList.add("popup");
+
+  // Create the popup content
+  var content = document.createElement("div");
+  content.classList.add("popup-content");
+  content.innerHTML = html;
+  popup.appendChild(content);
+
+  // Create the close button
+  var button = document.createElement("button");
+  button.classList.add("popup-close");
+  button.innerText = "Close";
+  button.addEventListener("click", function() {
+    // Add the "fade-out" class to the popup
+    popup.classList.add("fade-out");
+
+    // Remove the popup from the document after the fade out animation is complete
+    setTimeout(function() {
+      popup.remove();
+    }, 500);
+  });
+  content.appendChild(button);
+
+  // Add the popup to the document body
+  document.body.appendChild(popup);
+
+  // Add the "fade-in" class to the popup
+  setTimeout(function() {
+    popup.classList.add("fade-in");
+  }, 10);
+}
+
+createPopup("<h1>Hello!</h1><p>This is a popup with some <strong>HTML</strong> content.</p>");
