6b3913053f9163f9a3fbe31afb62e2e6fa5aa756
Adlen CHERIF <adlencherif29@gmail.com>
2025-09-17 15:19:03 +0200

    feat: Add placeUrl to EducationCard and update localization files

--- components/EducationSection.vue
+++ components/EducationSection.vue
@@ -2,7 +2,7 @@
   <div class="mt-16 flex flex-col gap-8">
     <SectionHeader :title="$t('Section.Education.Title')" :description="$t('Section.Education.Description')" />
     <EducationCard v-for="(education, index) in $tm('SectionEducationList')" :key="index" :title="$rt(education.Title)"
-      :year="$rt(education.Year)" :place="$rt(education.Place)"
+      :year="$rt(education.Year)" :place="$rt(education.Place)" :placeUrl="education.PlaceUrl ? $rt(education.PlaceUrl) : null"
       :keysInformations="education.KeysInformations.map(info => $rt(info))" />
   </div>
 </template>
