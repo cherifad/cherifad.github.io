569677655e176aa9076a5240f2e91f4934f65b8d
Adlen CHERIF <adlencherif29@gmail.com>
2025-09-18 14:18:23 +0200

    feat: Enhance error handling with localized messages and improved UI

--- error.vue
+++ error.vue
@@ -11,23 +11,20 @@ const handleError = () => clearError({ redirect: "/" });
 
 <template>
   <div class="flex flex-col items-center justify-center h-screen gap-4">
-    <h1
-      class="lg:text-8xl sm:text-6xl text-4xl text-[#dadada] font-normal uppercase"
-    >
+    <h1 v-if="error?.statusCode" class="lg:text-8xl sm:text-6xl text-4xl text-[#dadada] font-normal uppercase">
       {{ error?.statusCode }}
     </h1>
-    <p class="lg:text-2xl sm:text-xl text-lg text-center text-[#dadada] font-light">
-      {{ error?.statusMessage || "An unexpected error has occurred." }}
+    <p class="lg:text-4xl sm:text-3xl text-2xl text-center text-[#dadada] font-semibold">
+      {{ $t('Error.Title') }}
     </p>
-    <a href="#contact" class="text-center lg:text-start w-full lg:w-fit">
-      <Button
-        class="w-full"
-        text="Clear Error & Go Home"
-        :isBlack="false"
-        icon="mdi:arrow-top-right"
-        transition="top-right"
-        @click="handleError"
-      />
-    </a>
+    <p class="lg:text-2xl sm:text-xl text-lg text-center text-[#dadada] font-light">
+      {{ $t('Error.Message') }}
+    </p>
+    <p v-if="error?.statusMessage" class="lg:text-2xl sm:text-xl text-lg text-center text-[#dadada] font-light">
+      --> {{ error?.statusMessage }} <-- </p>
+        <div class="text-center lg:text-start w-full lg:w-fit">
+          <Button class="w-full" :text="$t('Error.Button')" :isBlack="false" icon="mdi:arrow-top-right"
+            transition="top-right" @click="handleError" />
+        </div>
   </div>
 </template>
