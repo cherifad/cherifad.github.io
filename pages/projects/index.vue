1f329a321a145303e46072ed54be0679f35a6ff3
Adlen CHERIF <adlencherif29@gmail.com>
2025-09-18 14:27:45 +0200

    feat: Add projects page with project listing and header component

---
+++ pages/projects/index.vue
@@ -0,0 +1,27 @@
+<template>
+    <div id="head" class="lg:min-h-[40vh] min-h-[20vh] rounded-b-3xl lg:px-16 px-9 flex flex-col">
+        <Header />
+        <div class="flex-1 flex justify-between my-16">
+            <div class="lg:w-2/3">
+                <h1 class="lg:text-8xl sm:text-6xl text-4xl font-semibold uppercase lin-grad">
+                    Projets
+                </h1>
+                <p class="md:text-2xl lg:w-11/12 font-light">
+                    Découvrez une sélection de projets sur lesquels j'ai travaillé, mettant en avant mes compétences et
+                    mon expertise dans le développement web et logiciel.
+                </p>
+            </div>
+        </div>
+    </div>
+    <div class="flex flex-wrap justify-center mt-16 lg:px-16 px-9">
+        <ProjectCard v-for="(project, index) in $tm('SectionProjectsList')" :key="index" :title="$rt(project.Title)"
+            :description="$rt(project.Description)" :link="project.Link"
+            :technologies="project.Technologies.map(technology => ({ name: $rt(technology.Name), icon: technology.Icon }))"
+            :image="$rt(project.Image)" :year="$rt(project.Year)" :slug="$rt(project.Slug)" />
+    </div>
+</template>
+
+<script setup>
+import Header from "@/components/nav/Header.vue";
+
+</script>
